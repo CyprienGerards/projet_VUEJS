3c6a5a3ca93a1d481ab1c9abe455893fb7ff49b0
Cyprien Gerards <gerards.cypri@gmail.com>
2025-06-21 11:34:13 +0200

    first vue, logo, Name

---
+++ src/components/main.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1></h1>
+    <h3>
+      <button class="green" type="button">Start Quizz</button>
+    </h3>
+  </div>
+</template>
+<style>
+h3 {
+position: left;
+}
+</style>
