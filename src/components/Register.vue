52c516a0a508775cd179e70606e807a5f70b7fa4
Cyprien Gerards <gerards.cypri@gmail.com>
2025-08-24 11:03:42 +0200

    add username register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,7 +6,7 @@ const password = ref('');
 const confirmpassword = ref('');
 const error = ref('');
 const success = ref('');
-
+const username =ref ('');
 
 
 function Register(){
@@ -14,7 +14,7 @@ function Register(){
     error.value = '';
     success.value = '';
     /* error sucess check */
-    if (!email.value || !password.value|| !confirmpassword.value){
+    if (!email.value || !password.value|| !confirmpassword.value|| !username.value){
             error.value = 'Please fill all fields';
             return;
         } 
@@ -28,6 +28,10 @@ function Register(){
 <template>
     <div>
         <h1>Register</h1>
+        <div class="form-inputs">
+            <label for="username">Username :</label>
+            <input v-model="username" id="username" type="username" placeholder="Enter a username">
+        </div>
         <div class="form-inputs">
             <label for="email">Email :</label>
             <input v-model="email" id="email" type="email" placeholder="Enter your email">
@@ -45,3 +49,16 @@ function Register(){
         <button @click="Register">Register</button>
     </div>
 </template>
+<style>
+.form-inputs {
+    padding-bottom: 10px;
+}
+
+label {
+    padding-right: 10px;
+    font-size: 1rem;
+}
+button{
+    font-size:1rem;
+}
+</style>
